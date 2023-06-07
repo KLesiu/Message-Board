cdc619191af70e7fc97ff18c5d74013e72ee5ed3
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-06-07 20:24:26 +0200

    Loop through the messages and show them in the main page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+ ];
+  res.render('index', { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = router;
