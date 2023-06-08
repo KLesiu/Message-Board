9cfe8f9db0e601ea2bf5455d70619f37c13c8227
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-06-08 02:08:53 +0200

    Add form and routes

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  },
+  {
+    text: "HIIII",
+    user: "Reece",
+    added: new Date()
+  }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
- ];
+  
   res.render('index', { title: "Mini Messageboard", messages: messages });
 });
+router.get('/new',function(req,res,next){
+  res.render("form",{})
+})
+router.post('/new',function(req,res,next){
+  const user = req.body.user
+  const msg = req.body.msg
+  messages.push({text: msg, user: user, added: new Date()})
+  res.redirect('/')
+})
 
 module.exports = router;
